1b6444f64c260351105388f08744ac743a7bd0ef
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-03-27 17:41:05 +0800

    update api url

--- src/data/api.js
+++ src/data/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const http = axios.create({
-    baseURL: 'http://localhost:3001/',
+    baseURL: 'https://task-manager-two-mocha.vercel.app/',
     headers: {
         'Access-Control-Allow-Headers' : '*',
         'Access-Control-Allow-Origin' : '*'
